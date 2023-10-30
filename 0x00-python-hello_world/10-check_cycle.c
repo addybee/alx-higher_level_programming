e98f48c733f35d3cf097a1c3ae2ea55c70b896fe
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2023-10-30 16:10:30 -0700

    first commit for task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it.
+ * @list: the head of the list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *node;
+
+	if (list == NULL)
+		return (0);
+	else if (list->next == NULL)
+		return (0);
+	node = list;
+	while (node->next != list)
+	{
+		node = node->next;
+		if (node == NULL)
+			return (0);
+	}
+	return (1);
+}
