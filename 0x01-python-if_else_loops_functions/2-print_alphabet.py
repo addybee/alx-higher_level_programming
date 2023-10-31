0d227cd3a4c83de5ae41a25594166f438ce990fb
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2023-10-31 13:33:15 -0700

    first commit for task4 and edited task 2 to pycodestyle

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 for char in range(97, 123):
-    if char == 101 or char == 113:
-        continue
-    print("{:c}".format(char), end = "")
+    print("{:c}".format(char), end="")
