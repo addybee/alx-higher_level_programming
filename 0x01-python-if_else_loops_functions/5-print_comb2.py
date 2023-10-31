56d7ca64c99b8a7d50538a3f4fd1ddcaa318a836
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2023-10-31 14:01:14 -0700

    remove the last comma behind 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for num in range(100):
-    print("{:0<2d}".format(num), end=", ")
-print("")
+    if num == 99:
+        print("{:<02d}".format(num))
+        break
+    print("{:<02d}".format(num), end=", ")
