f97d4275ac2bbf60a4c0e4d9489c97973d4aa715
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2023-11-02 00:36:58 -0700

    first commit for module and import project

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# imports the function def add(a, b): from the file add_0.py and prints
+# the result of the addition 1 + 2 = 3
+from add_0 import add
+a = 1
+b = 2
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
