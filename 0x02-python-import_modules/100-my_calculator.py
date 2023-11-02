03ed48a4579a4e4683e71cbfdb41058f9479f5b6
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2023-11-02 10:10:04 -0700

    edited syntax of task 2 and task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __main__ == "__main__":
+if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
     if len(argv)!= 4:
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 # import argv from sys
 if __name__ == "__main__":
-    import sys
-    argv = sys.argv
-    length = len(argv) - 1
-    if length == 0:
-        print(f"{length} arguments.")
-    else:
-        if length == 1:
-            print(f"{length:d} argument:\n1: {argv[length]}")
-        else:
-            print(f"{length:d} argument:")
-            for i, item in enumerate(argv[1:]):
-                i += 1
-                print(f"{i}: {item}")
+    from sys import argv
+    length = len(argv[1:])
+    print("{:d} {:s}{:s}".format(length, "arguments" if length != 1 else
+        "argument", ":" if length != 0 else "."))
+    if length > 0:
+        for index, item in enumerate(argv[1:]):
+            index += 1
+            print(f"{index}: {item}")
