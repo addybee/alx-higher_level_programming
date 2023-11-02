14c52f4f9a747f2ce2efa339dd9111a3401576cd
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2023-11-02 10:59:35 -0700

    edited task 6 error on division process

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,18 +2,23 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
-    if len(argv) != 4:
+    if len(argv[1:]) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     operators = ["+", "-", "*", "/"]
     if argv[2] not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
+    answer = 0
     if argv[2] == operators[0]:
-        print(f"{argv[1]} + {argv[3]} = {add(int(argv[1]), int(argv[3]))}")
+        answer = add(int(argv[1]), int(argv[3]))
+        print(f"{argv[1]} + {argv[3]} = {answer}")
     elif argv[2] == operators[1]:
-        print(f"{argv[1]} - {argv[3]} = {sub(int(argv[1]), int(argv[3]))}")
+        answer = sub(int(argv[1]), int(argv[3]))
+        print(f"{argv[1]} - {argv[3]} = {answer}")
     elif argv[2] == operators[2]:
-        print(f"{argv[1]} * {argv[3]} = {mul(int(argv[1]), int(argv[3]))}")
-    elif argv[2] == operator[3]:
-        print(f"{argv[1]} / {argv[3]} = {div(int(argv[1]), int(argv[3]))}")
+        answer = mul(int(argv[1]), int(argv[3]))
+        print(f"{argv[1]} * {argv[3]} = {answer}")
+    elif argv[2] == operators[3]:
+        answer = div(int(argv[1]), int(argv[3]))
+        print(f"{argv[1]} / {argv[3]} = {answer}")
