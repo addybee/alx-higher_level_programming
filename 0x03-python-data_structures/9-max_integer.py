1906e09b69aed200b5de0de96176dd517de60835
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2023-11-04 13:42:30 -0700

    fix check 6 and 11

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    maxi = 0
+    maxi = my_list[0] 
     for i in range(len(my_list)):
         if maxi < my_list[i]:
             maxi = my_list[i]
