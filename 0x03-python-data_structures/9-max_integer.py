43356c305ea7980ff1fdfe815a61559a775c48a8
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2023-11-04 13:24:20 -0700

    attempt task 8 and 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not list:
+        return None
+    maxi = 0
+    for i in range(len(my_list)))
+        if maxi < my_list[i]:
+            maxi = my_list[i]
+    return maxi
