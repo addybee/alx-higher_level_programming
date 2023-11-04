ca3279d38121f2a68dc395375df64976581036f2
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2023-11-04 12:08:14 -0700

    fix the failed checks and attempt task 7

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    nl = len(my_list)
-    for idx in range(nl):
-        print("{:d}".format(my_list[nl - 1 - idx]))
+    if not my_list:
+        return 
+    for integer in reversed(my_list):
+        print("{:d}".format(integer)
---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    lst_a = list(tuple_a)
+    lst_b = list(tuple_b)
+    while len(lst_a) < 2:
+        lst_a.append(0)
+    while len(lst_b) < 2:
+        lst_b.append(0)
+    return tuple(i + j for i, j in zip(lst_a, lst_b))
