0ee160c02e275af5d43825e97fec0880fc4e9237
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2023-11-04 00:10:28 -0700

    fix the failed checks

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 #  creates a copy of the string, removing the character at the position n
 def remove_char_at(str, n):
-    cpy_str = str[:n] + str[n + 1 :]
+    if n < 0:
+        return str
+    cpy_str = str[:n] + str[n + 1:]
     return cpy_str
