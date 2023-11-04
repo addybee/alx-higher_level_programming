5f8e135081385bcf1bf66031b38a10fbe693074a
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2023-11-04 12:56:52 -0700

    fix check 10

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,4 +6,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         lst_a.append(0)
     while len(lst_b) < 2:
         lst_b.append(0)
-    return tuple(i + j for i, j in zip(lst_a, lst_b))
+    return tuple(i + j for k, i, j in zip(lst_a[:2], lst_b[:2]))
