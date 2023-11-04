c85ee05a5fa03f990a3291e15c56ba48cfbab343
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2023-11-04 03:08:05 -0700

    fix the failed checks and attempt task 6

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for integer in reversed(my_list):
-        print("{:d}".format(integer))
+    nl = len(my_list)
+    for idx in range(nl):
+        print("{:d}".format(my_list[nl - 1 - idx]))
