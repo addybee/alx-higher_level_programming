7eb7f8db9b2149305d606a4b033d9ff2bc132acb
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2023-11-07 06:39:18 -0800

    Revert "attempted task 100"
    This reverts commit 66a6ff89072f345a3dec60b7bc731f927dc57807.
    revert to prev

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -16,7 +16,7 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int in);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
 int *arr_listint(const listint_t *h, unsigned int *len);
