6eff46cb181e608b3a230c822d8a7a1ebe63ddb0
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2023-11-08 15:37:14 -0800

    attemt task 8 and task 9

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary:
+    try:
         del a_dictionary[key]
+    except KeyError:
+        pass
     return a_dictionary
