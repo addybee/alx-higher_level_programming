8f74e9a6c861d0636b8d1f3b1811e9e3e3d2279b
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2023-11-08 17:55:23 -0800

    fix failed checks in task 102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,7 @@
 def complex_delete(a_dictionary, value):
     if a_dictionary is None:
         return
-    for item in a_dictionary:
+    for item in a_dictionary.copy:
         if a_dictionary[item] == value:
             del a_dictionary[item]
     return a_dictionary
