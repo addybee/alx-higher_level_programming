4b11e47fa181b93f1b7708c8a5ee1f9c25025167
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2023-11-08 17:23:04 -0800

    fix failed checks in advance task

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,4 +2,4 @@
 def weight_average(my_list=[]):
     if len(my_list) == 0:
         return 0
-    return sum(k[0] * k[1] for x in my_list]) / sum([k[1] for k in my_list])
+    return sum([k[0] * k[1] for x in my_list]) / sum([k[1] for k in my_list])
