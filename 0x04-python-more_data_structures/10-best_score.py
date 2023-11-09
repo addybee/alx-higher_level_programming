be1642fd93e0e0f68cf2e9ac2d569555bda9cbd6
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2023-11-08 16:19:02 -0800

    fix task 10 and did task 11

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
     key = list(a_dictionary)[0]
     max = a_dictionary[key]
     for k, v in a_dictionary.items():
-        if v > max:
+        if max < v:
             max = v
             key = k
     return key
