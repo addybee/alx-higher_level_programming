331103937b0fc8187b3b559ac6577e18fcee136d
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2023-11-20 15:47:42 -0800

    fix and task 101

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import stderr
+
+def safe_function(fct, *args):
+    try:
+        res = fct(*args)
+    except BaseException as e:
+        print("Exception: {}".format(e), file=stderr)
+        res = None
+    finally:
+        return res
