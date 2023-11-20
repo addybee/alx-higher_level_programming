92fe06f33689aee3698d31e5acb8fcf7e8446818
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2023-11-20 15:09:45 -0800

    fix and task 4

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     res = 0
     try:
         res = a / b
-    except:
+    except ZeroDivisionError:
         res = None
     finally:
         print("Inside result: {}".format(res))
---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new = []
+    for idx in range(list_length):
+        try:
+            res = my_list_1[idx] / my_list_2[idx]
+        except ZeroDivisionError:
+            res = 0
+            print("division by 0")
+        except (ValueError, TypeError):
+            res = 0
+            print("wrong type")
+        except IndexError:
+            res = 0
+            print("out of range")
+        finally:
+            new.append(division)
+    return new
