6fb36f746e4f406355d44afdec75782b6ed6fd9a
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2023-11-21 15:18:55 -0800

    task 4 and 5

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -17,5 +17,22 @@ class Square:
         self.__size = size
 
     def area(self):
-        """ retrieve the square area"""
+        """retrieve the area of square"""
         return self.__size ** 2
+
+    @property
+    def size(self):
+        """retrieve the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """set the size of the square
+        Args:
+            value (int): new size to be set
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""define a class Square"""
+
+
+class Square:
+    """defines a square by by private instance"""
+    def __init__(self, size=0):
+        """Instantiation with size (no type/value verification)
+
+        Args:
+            size (int): the size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """retrieve the area of square"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """retrieve the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """set the size of the square
+        Args:
+            value (int): new size to be set
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
