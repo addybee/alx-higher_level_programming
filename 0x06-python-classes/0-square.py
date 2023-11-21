11cf5fb7c91676c6b42fc86b07f815c2f9753d0f
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2023-11-21 13:25:05 -0800

    fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """define a class square"""
 
+
 class Square:
     """empty class"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """define a class Square"""
+
+
 class Square:
     """defines a square by by private instance"""
-
     def __init__(self, size):
         """Instantiation with size (no type/value verification)
 
