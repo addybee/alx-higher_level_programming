a30b701fc3a9d389c4fdfeb2e15cb8ca75d751fd
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2024-02-05 15:59:04 -0800

    fix bug task 1 and 2

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-"""class MyList that inherits from list:
-"""
+"""class MyList that inherits from list"""
 
 
 class MyList(list):
-    """class MyList that inherits from list
-
-    Args:
-        list (parent): parent or super class
-    """
     def print_sorted(self):
-        """prints the list, but sorted (ascending sort)
-        """
+        """prints the list, but sorted (ascending sort)"""
         print(sorted(self))
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,15 +5,4 @@ of the specified class
 
 
 def is_same_class(obj, a_class):
-    """_summary_
-
-    Args:
-        obj (object): object to be check
-        a_class (class): the class
-
-    Returns:
-        bool: True if instance else False
-    """
-    if isinstance(obj, a_class):
-        return True
-    return False
+    return True if isinstance(obj, a_class) else False
