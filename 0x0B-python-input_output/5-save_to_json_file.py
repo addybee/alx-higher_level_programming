d8c662f2cf20832b7e89d28cade1f30c8420a820
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2024-02-06 06:47:14 -0800

    task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -3,7 +3,7 @@
 from json import dump
 
 
-def to_json_string(my_obj, filename):
+def save_to_json_file(my_obj, filename):
     """ this function writes an object to file """
 
     with open(filename, mode="w", encoding="utf-8") as myFile:
