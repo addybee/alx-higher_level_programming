9e5046f9d51ff5cb861d91ee33d31855c2ab3aac
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2024-02-05 18:48:35 -0800

    fix bug

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """define an class Square that inherits from Rectangle class"""
-Rectangle = __import__('9-rectangle.py').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
