250cdd6c72dbf6935342c6072672263a08be3ce9
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2024-03-13 18:38:03 -0700

    fixed error

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,8 @@
 --  lists all Comedy shows in the database hbtn_0d_tvshows
 
-SELECT tv_shows.title FROM tv_shows
-LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id = (SELECT id FROM tv_genres
-	WHERE name = "Comedy")
+SELECT tv_shows.title 
+FROM tv_shows, tv_show_genres, tv_genres
+WHERE tv_genres.name = "Comedy"
+AND tv_shows.id = tv_show_genres.show_id
+AND tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title ASC;
