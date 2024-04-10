a9e12809745668c4fdeafb54f06504b3dc04340a
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-04-10 05:53:03 +0100

    task9

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -9,14 +9,12 @@ if (file1 && file2 && file3) {
   fs.readFile(file1, 'utf8', (err, data1) => {
     if (err) {
       console.error(`Error reading ${file1}: ${err.message}`);
-      return;
     }
 
     // Read content from the second source file
     fs.readFile(file2, 'utf8', (err, data2) => {
       if (err) {
         console.error(`Error reading ${file2}: ${err.message}`);
-        return;
       }
 
       // Concatenate the contents of both files
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,4 +1,4 @@
-#!/usr/bin//node
+#!/usr/bin/node
 
 let num = 0;
 exports.logMe = function (item) {
