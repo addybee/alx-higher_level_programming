67d0f6c30fb6208ae296de6e7c96cae81b12c543
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-04-10 05:45:07 +0100

    task 9 and 102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,32 +1,34 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 
-const [, , file1, file2, file3] = process.argv;
-
-// Read content from the first source file
-fs.readFile(file1, 'utf8', (err, data1) => {
-  if (err) {
-    console.error(`Error reading ${file1}: ${err.message}`);
-    return;
-  }
-
-  // Read content from the second source file
-  fs.readFile(file2, 'utf8', (err, data2) => {
+const { argv } = require('node:process');
+const [, , file1, file2, file3] = argv;
+if (file1 && file2 && file3) {
+  // Read content from the first source file
+  fs.readFile(file1, 'utf8', (err, data1) => {
     if (err) {
-      console.error(`Error reading ${file2}: ${err.message}`);
+      console.error(`Error reading ${file1}: ${err.message}`);
       return;
     }
 
-    // Concatenate the contents of both files
-    const result = data1 + data2;
-
-    // Write the concatenated content to the destination file
-    fs.writeFile(file3, result, (err) => {
+    // Read content from the second source file
+    fs.readFile(file2, 'utf8', (err, data2) => {
       if (err) {
-        console.error(`Error writing to ${file3}: ${err.message}`);
+        console.error(`Error reading ${file2}: ${err.message}`);
         return;
       }
-      console.log(`Files ${file1} and ${file2} successfully concatenated to ${file3}`);
+
+      // Concatenate the contents of both files
+      const result = data1 + data2;
+
+      // Write the concatenated content to the destination file
+      fs.writeFile(file3, result, (err) => {
+        if (err) {
+          console.error(`Error writing to ${file3}: ${err.message}`);
+          return;
+        }
+      });
     });
   });
-});
+}
