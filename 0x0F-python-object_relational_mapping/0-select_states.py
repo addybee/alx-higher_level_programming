533c1580eca52477d3d1d12b46c11ec8bfb9ded0
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-04-13 23:20:47 +0100

    first commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" lists all states from the database hbtn_0e_0_usa
+"""
+if __name__ == "__main__":
+	import MySQLdb
+	from sys import argv
+
+	db = MySQLdb.connect(user=argv[1], password=argv[2], database=argv[3],
+			port=3306, host="localhost")
+	cur = db.cursor()
+	cur.execute("SELECT name FROM hbtn_0e_0_usa ORDER BY id")
+	rows = cur.fetchall()
+	for row in rows:
+		print (row)
+	cur.close()
+    	db.close()
+        git aadd
