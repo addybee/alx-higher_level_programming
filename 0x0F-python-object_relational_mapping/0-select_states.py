a1893dee9aaa567a1845dc1d24624f27d355057e
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-04-13 16:03:58 -0700

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """ lists all states from the database hbtn_0e_0_usa
 """
-if __name__ == "__main__":
-	import MySQLdb
-	from sys import argv
 
-	db = MySQLdb.connect(user=argv[1], password=argv[2], database=argv[3],
-			port=3306, host="localhost")
-	cur = db.cursor()
-	cur.execute("SELECT name FROM hbtn_0e_0_usa ORDER BY id")
-	rows = cur.fetchall()
-	for row in rows:
-		print (row)
-	cur.close()
-    	db.close()
-        
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(user=argv[1], password=argv[2], database=argv[3],
+                         port=3306, host="localhost")
+    cur = db.cursor()
+    cur.execute("""SELECT id, name FROM states ORDER BY id""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
