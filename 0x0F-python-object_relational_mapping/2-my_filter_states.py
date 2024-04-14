93c0ea89262afd6b5ffb5971956e76301479e6e6
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-04-13 18:09:59 -0700

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" displays all values in the states table of hbtn_0e_0_usa
+    where name matches the argument.
+"""
+
+
+if __name__ == "__main__":
+
+    from sys import argv
+    import MySQLdb
+
+    db = MySQLdb.connect(user=argv[1], password=argv[2], database=argv[3],
+                         port=3306, host="localhost")
+    cur = db.cursor()
+    cur.execute("""SELECT id, name FROM states WHERE name=%s
+                ORDER BY id""", (argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
