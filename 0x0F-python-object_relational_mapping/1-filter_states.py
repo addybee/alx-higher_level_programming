7edb1532837e7152bdad8399d894906b88005819
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-04-13 17:50:56 -0700

    task1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""  lists all states with a name starting with N (upper N)
-	 from the database hbtn_0e_0_usa
+""" lists all states with a name starting with N (upper N)
+    from the database hbtn_0e_0_usa
 """
 
 
 if __name__ == "__main__":
-    
-    from sys import argv, path
-    print(path)
+
+    from sys import argv
     import MySQLdb
+
     db = MySQLdb.connect(user=argv[1], password=argv[2], database=argv[3],
                          port=3306, host="localhost")
     cur = db.cursor()
