a6acae7b9a71bca0307d4fb205376d2d4a51f19c
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-04-15 02:27:13 +0100

    task5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,27 +7,25 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    db = None
-    cur = None
     try:
 
         db = MySQLdb.connect(user=argv[1], password=argv[2], database=argv[3],
                              port=3306, host="localhost")
         cur = db.cursor()
-        query = """SELECT name FROM cities
-                WHERE state_id = (SELECT id FROM states WHERE name = %s)
-                ORDER BY id"""
-        size = cur.execute(query, (argv[4],))
+        query = """SELECT cities.name FROM cities
+                INNER JOIN states
+                ON cities.state_id = states.id
+                WHERE states.name = %s
+                ORDER BY cities.id"""
+        cur.execute(query, (argv[4],))
         rows = cur.fetchall()
+        size = len(rows)
         for row in rows:
             if row == rows[size - 1]:
                 print(row[0])
             else:
                 print(row[0], end=', ')
-    except MySQLdb.Error:
-        pass
-    finally:
-        if cur:
-            cur.close()
-        if db:
-            db.close()
+        cur.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print(e)
