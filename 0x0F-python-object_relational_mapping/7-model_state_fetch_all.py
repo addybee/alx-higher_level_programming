909e36ac5cb3e59e0c758b34312ab86a23ad14a9
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-04-15 03:43:15 +0100

    task 7 to 8 done

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    try:
+
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.\
+            format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+        
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        query = session.query(State).order_by(State.id)
+        for obj_row in query.all():
+            print("{}: {}".format(obj_row.id, obj_row.name))
+        engine.dispose()
+    except SQLAlchemyError as e:
+        print(e)
---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    try:
+
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.\
+            format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+        
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        first_state = session.query(State).order_by(State.id).first()
+        print("{}: {}".format(first_state.id, first_state.name))
+        session.close()
+        engine.dispose()
+    except SQLAlchemyError as e:
+        print(e)
