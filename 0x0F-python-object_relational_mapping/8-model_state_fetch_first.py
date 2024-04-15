3c90a21e3557be7105df31a9ceb5c1ae210efd3a
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-04-15 04:47:57 +0100

    added doctstring for module

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -19,8 +19,11 @@ if __name__ == "__main__":
         Base.metadata.create_all(engine)
         Session = sessionmaker(bind=engine)
         session = Session()
-        first_state = session.query(State).order_by(State.id).first()
-        print("{}: {}".format(first_state.id, first_state.name))
+        first_state = session.query(State).order_by(State.id).one_or_none()
+        if first_state:
+            print("{}: {}".format(first_state.id, first_state.name))
+        else:
+            print("Nothing")
         session.close()
         engine.dispose()
     except SQLAlchemyError as e:
