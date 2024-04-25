3600e0f11df7a796a6b2ad45eccd9bc0844b21fb
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-04-26 00:19:04 +0100

    docs added

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,14 @@
 
 
 def find_peak(list_of_integers):
+    """finds a peak
+
+    Args:
+        list_of_integers (list): the provided list
+
+    Returns:
+        int: a peak in the list
+    """
     sorted_list = sorted(list_of_integers)
     size = len(list_of_integers)
     if size == 0:
