853425360f385206b88f70ea94766ed912dc0a81
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-05-22 22:01:31 +0100

    fix minor issue

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,11 +2,10 @@
 
 const api = process.argv[2];
 const request = require('request');
-const ID = 18;
-const wedgeAntilles = `https://swapi-api.alx-tools.com/api/people/${ID}/`;
+const ID = '18';
 
 request.get(api, (error, response) => {
   if (error) throw error;
   const results = JSON.parse(response.body).results;
-  console.log(results.reduce((acc, result) => acc + result.characters.filter(character => character === wedgeAntilles).length, 0));
+  console.log(results.reduce((acc, result) => acc + result.characters.filter(character => character.includes(ID)).length, 0));
 });
